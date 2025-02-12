345ed3153af61fe05c7b006bcd1fb0fc8f7a5a85
en-dotcom <nawal.waqqas04@gmail.com>
2025-02-12 16:41:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "YAYAYAYYAYAYA I LOBE YOU :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,13 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "no",
+    "wait what?",
+    "baby no :(",
+    ":((",
+    ":(((",
+    "dies",
+    "PRESS YES PLEASEPLEASE",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
